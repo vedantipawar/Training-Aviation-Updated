7f1529417768a5e12dce15650d8574e5f991a5b9
Vedanti Pawar <vedantipawar0801@gmail.com>
2024-06-03 05:04:59 -0700

    Getting started SideNav

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div className = 'SideNav'>
+        <h1>
+            Logo
+        </h1>
+        <ul>
+            <li>
+                <a href= '/'> Home</a>
+            </li>
+        </ul>
+    </div>
+  )
+}
